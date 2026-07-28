f483f4506e31e352ddfc0309760a27b4bf5b5817
Matusalen Alves <matusalencosta@icloud.com>
2026-07-28 15:44:31 -0300

    Referencing the user's posts

--- src/main/java/com/matusalenalves/workshopmongo/configuration/Instantiation.java
+++ src/main/java/com/matusalenalves/workshopmongo/configuration/Instantiation.java
@@ -38,6 +38,9 @@ public class Instantiation implements CommandLineRunner {
         Post p2 = new Post(null, sdf.parse("23/03/2026"), "Bom dia.", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(p1, p2));
+
+        maria.getPosts().addAll(Arrays.asList(p1, p2));
+        userRepository.save(maria);
     }
 
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
