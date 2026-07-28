29ddfa669f8fce6be1487c3366ce41593cf44cfc
Matusalen Alves <matusalencosta@icloud.com>
2026-07-28 16:44:56 -0300

    Simple query with @Query

--- src/main/java/com/matusalenalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/matusalenalves/workshopmongo/repository/PostRepository.java
@@ -2,11 +2,15 @@ package com.matusalenalves.workshopmongo.repository;
 
 import com.matusalenalves.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title':  { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String title);
+
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/com/matusalenalves/workshopmongo/services/PostService.java
+++ src/main/java/com/matusalenalves/workshopmongo/services/PostService.java
@@ -18,6 +18,6 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String title){
-        return postRepository.findByTitleContainingIgnoreCase(title);
+        return postRepository.searchTitle(title);
     }
 }
